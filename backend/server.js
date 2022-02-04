39d506280b6f616c701860454b7b4e1ee24b5c1b
unknown <chepurnojv@gmail.com>
2022-02-04 11:10:39 +0200

    Modeling Our Data:

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import products from "./data/products.js";
+import colors from "colors";
 
 dotenv.config();
 
@@ -25,6 +26,9 @@ app.get("/api/products/:id", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(
-  5000,
-  console.log(`Server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  PORT,
+  console.log(
+    `Server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
 );
